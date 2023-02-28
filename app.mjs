b260344bad08428240a77b28a2d270132eda7e5b
Avi Mamenko <avimamenko@gmail.com>
2023-02-28 00:36:29 -0500

    Added dockerignore, lambda warmer, and cleaned up unused dependencies

--- app.mjs
+++ app.mjs
@@ -1,5 +1,7 @@
-import { createMashup } from "./functions/mix/createMashup";
+const { createMashup } = require("./functions/mix/createMashup");
+const warmer = require("lambda-warmer");
 
 export const lambdaHandler = async (event, context) => {
+  if (await warmer(event)) return "warmed";
   createMashup();
 };
